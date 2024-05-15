6c47d573058abacb84a50efd937fe0fe9a5fcf34
Lune <lolidrop@icloud.com>
2024-05-15 12:46:48 -0700

    update

--- ModLoaderFix/ModLoaderFixPlugin.cs
+++ ModLoaderFix/ModLoaderFixPlugin.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using BepInEx;
+using BepInEx.Configuration;
 using Game.Services;
 using Game.Services.Filesystem;
 using Game;
 using HarmonyLib;
 using Steamworks;
+using UnityEngine;
 
 namespace ModLoaderFix
 {
@@ -30,6 +32,7 @@ namespace ModLoaderFix
         }
         public static Dictionary<PublishedFileId_t,WorkshopModInfo> WorkshopModInfos { get; private set; }
         public static bool SteamClientStarted;
+        public static ConfigEntry<KeyCode> ReloadModsKey;
         private void Start()
         {
             _harmony = new Harmony("com.bepinex.plugins.modloaderfix");
@@ -38,6 +41,88 @@ namespace ModLoaderFix
             WorkshopPath = System.IO.Path.Combine(Paths.GameRootPath ,"../../workshop/content/423580");
             LogInfo("ModLoaderFix loaded");
             WorkshopModInfos= GetWorkshopDirectories();
+       //     FileWatcherWorkshopDirectory();
+            ReloadModsKey = Config.Bind("General", "ReloadModsKey", KeyCode.F8);
+        }
+
+        private void Update()
+        {
+            if (!IsMainMenu)
+            {
+                return;
+            }
+
+            if (Input.GetKeyDown(ReloadModsKey.Value))
+            {
+                LogInfo("Reloading mods");
+                ServiceContext.mods.Release();
+                ServiceContext.mods.Initialize();
+                LogInfo("Mods reloaded");
+            }
+        }
+
+        public static ServiceContext ServiceContext => Game.Game.serv;
+        FileSystemWatcher watcher;
+        public static bool IsMainMenu => ServiceContext.scenarios.sessionType == ScenarioService.Type.Unknown;
+ //       public void FileWatcherWorkshopDirectory()
+//       {
+//           if (watcher != null)
+//           {
+//               watcher.EnableRaisingEvents = false;
+//               watcher.Dispose();
+//           }
+//           watcher = new FileSystemWatcher(WorkshopPath);
+//           watcher.NotifyFilter = NotifyFilters.DirectoryName ;
+//           watcher.Created += (sender, args) =>
+//           {
+//               if (!IsWorkshopModDirectory(args.FullPath))
+//               {
+//                   return;
+//               }
+//               LogInfo($"Directory created {args.Name}");
+//               var info = new WorkshopModInfo(args.FullPath);
+//               if (info.IsWorkshopMod)
+//               {
+//                   WorkshopModInfos[info.Id] = info;
+//                   if (!IsMainMenu)
+//                   {
+//                       return;
+//                   }
+//                   var modDefinition=info.ParseModInfo();
+//                   ServiceContext.saveload.player.prefs.enabledmods[info.Id.m_PublishedFileId.ToString()] = true;
+//                   SteamModsProviderStartLoadingModDefinitions.rootpaths[info.Id.m_PublishedFileId.ToString()] = info.RootPath;
+//                   SteamModsProviderStartLoadingModDefinitions.moddefs.Add(modDefinition);
+//                   SteamModsProviderStartLoadingModDefinitions.modDictionary[modDefinition.modid.id] = modDefinition;
+//                   
+//               }
+//           };
+//           watcher.Deleted += (sender, args) =>
+//           {
+//               if (!IsWorkshopModDirectory(args.FullPath))
+//               {
+//                   return;
+//               }
+//               LogInfo($"Directory deleted {args.Name}");
+//               var info = new WorkshopModInfo(args.FullPath);
+//               if (info.IsWorkshopMod)
+//               {
+//                   WorkshopModInfos.Remove(info.Id);
+//                   if (!IsMainMenu)
+//                   {
+//                       return;
+//                   }
+//              
+//                   ServiceContext.saveload.player.prefs.enabledmods.Remove(info.Id.m_PublishedFileId.ToString());
+//                   SteamModsProviderStartLoadingModDefinitions.rootpaths.Remove(info.Id.m_PublishedFileId.ToString());
+//                   SteamModsProviderStartLoadingModDefinitions.moddefs.RemoveAll(modDefinition => modDefinition.modid.id == info.Id.m_PublishedFileId.ToString());
+//               }
+//           };
+//           watcher.EnableRaisingEvents = true;
+ //       }
+        public bool IsWorkshopModDirectory(string directory)
+        {
+            var directoryName = Path.GetFileName(directory);
+            return Directory.Exists(Path.Combine(WorkshopPath, directoryName));
         }
         public static Dictionary<PublishedFileId_t,WorkshopModInfo> GetWorkshopDirectories()
         {
@@ -53,7 +138,7 @@ namespace ModLoaderFix
                      LogWarning($"Invalid workshop directory {directoryName}");
                    continue;
                }
-               LogInfo($"Found workshop directory {directoryName}");
+          //     LogInfo($"Found workshop directory {directoryName}");
                workshopModInfos.Add(info.Id,info);
            }
            return workshopModInfos;
--- ModLoaderFix/SteamModsProviderStartLoadingModDefinitions.cs
+++ ModLoaderFix/SteamModsProviderStartLoadingModDefinitions.cs
@@ -8,15 +8,20 @@ namespace ModLoaderFix
     [HarmonyPatch(typeof(SteamModsProvider), "StartLoadingModDefinitions")]
     public static class SteamModsProviderStartLoadingModDefinitions
     {
+        public static Dictionary<string, ModDefinition> modDictionary;
+        public static Dictionary<string, string> rootpaths;
+        public static List<ModDefinition> moddefs;
+
         public static bool Prefix(SteamModsProvider __instance)
         {
             ModLoaderFixPlugin.LogInfo("SteamModsProvider.StartLoadingModDefinitions");
             var modService = Game.Game.serv.mods;
-            var modDictionary = Traverse.Create(modService).Field<Dictionary<string, ModDefinition>>("_mods").Value;
+             modDictionary = Traverse.Create(modService).Field<Dictionary<string, ModDefinition>>("_mods").Value;
             var traverse = Traverse.Create(__instance);
-            var rootpaths = traverse.Field<Dictionary<string, string>>("rootpaths").Value;
-            var moddefs = traverse.Field<List<ModDefinition>>("moddefs").Value;
-            
+             rootpaths = traverse.Field<Dictionary<string, string>>("rootpaths").Value;
+             moddefs = traverse.Field<List<ModDefinition>>("moddefs").Value;
+             var player = ModLoaderFixPlugin.ServiceContext.saveload.player;
+            var prefs = player.prefs;
             var values = new List<WorkshopModInfo>(ModLoaderFixPlugin.WorkshopModInfos.Values);
             for (var i = 0; i < ModLoaderFixPlugin.WorkshopModInfos.Count; i++)
             {
@@ -24,6 +29,12 @@ namespace ModLoaderFix
                 var modDefinition =  info.ParseModInfo();
                 var id = modDefinition.modid.id;
                 moddefs.Add(modDefinition);
+                if (!prefs.enabledmods.ContainsKey(id))
+                {
+                    
+                prefs.enabledmods[id] = true;
+                ModLoaderFixPlugin.LogInfo($"[Active] mod {id}  name: {modDefinition.name} description: {modDefinition.description}");
+                }
                 if (modDictionary.ContainsKey(id))
                 {
                     ModLoaderFixPlugin.LogInfo($"Mod {id} already loaded");
@@ -31,6 +42,8 @@ namespace ModLoaderFix
                 modDictionary[id] = modDefinition;
                 rootpaths[id] = info.RootPath;
             }
+
+            player.SavePrefs();
             
             return false;
         }
